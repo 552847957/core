934840911235308f16644393dffa1138c5b7f508
Andrea Del Bene <an.delbene@gmail.com>
2013-09-04 19:00:57 +0200

    Code refactoring clean-up

--- jdk-1.6-parent/wicketstuff-restannotations-parent/restannotations/src/main/java/org/wicketstuff/rest/heteaos/annotations/ClassHandler.java
+++ jdk-1.6-parent/wicketstuff-restannotations-parent/restannotations/src/main/java/org/wicketstuff/rest/heteaos/annotations/ClassHandler.java
@@ -5,5 +5,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ClassHandler {
-	Class<?>[] value();
+	Class<?> value();
 }
--- jdk-1.6-parent/wicketstuff-restannotations-parent/restannotations/src/main/java/org/wicketstuff/rest/heteaos/resource/AbstractHateoasResource.java
+++ jdk-1.6-parent/wicketstuff-restannotations-parent/restannotations/src/main/java/org/wicketstuff/rest/heteaos/resource/AbstractHateoasResource.java
@@ -1,44 +1,29 @@
 package org.wicketstuff.rest.heteaos.resource;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.wicket.authroles.authorization.strategies.role.IRoleCheckingStrategy;
 import org.wicketstuff.rest.contenthandling.IWebSerialDeserial;
 import org.wicketstuff.rest.heteaos.IHateaosUriResolver;
 import org.wicketstuff.rest.heteaos.annotations.ClassHandler;
 import org.wicketstuff.rest.resource.AbstractRestResource;
-import org.wicketstuff.rest.utils.http.HttpMethod;
 
 public abstract class AbstractHateoasResource<T extends IWebSerialDeserial> extends AbstractRestResource<T> {
-	private final Map<HttpMethod, Integer> httpMethodAndSegNumber = new HashMap<HttpMethod, Integer>()
-    {
-		{
-			put(HttpMethod.POST, 0);
-			put(HttpMethod.PUT, 0);
-			put(HttpMethod.GET, 1);
-			put(HttpMethod.DELETE, 1);
-		}
-	};
+	private final IHateaosUriResolver uriResolver;
 	
 	public AbstractHateoasResource(T serialDeserial, IHateaosUriResolver uriResolver) {
-		super(serialDeserial);
-		loadHateaosData(uriResolver);
+		this(serialDeserial, null, uriResolver);
 	}
 
 	public AbstractHateoasResource(T serialDeserial, IRoleCheckingStrategy roleCheckingStrategy, IHateaosUriResolver uriResolver) {
 		super(serialDeserial, roleCheckingStrategy);
-		loadHateaosData(uriResolver);
+		this.uriResolver = uriResolver;
+		
+		loadHateaosLinks();
 	}
 
-	private void loadHateaosData(IHateaosUriResolver uriResolver) {
+	private void loadHateaosLinks() {
 		ClassHandler classHandler = getClass().getAnnotation(ClassHandler.class);
-		Class<?>[] supportedClasses = classHandler.value();
+		Class<?> supportedClasses = classHandler.value();
 		
-		for (int i = 0; i < supportedClasses.length; i++) {
-			Class<?> clazz = supportedClasses[i];
-			
-			
-		}
+		
 	}
 }
